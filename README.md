1c6689413f83044fb53c47bbac9f29e9344ec952
Peter Mühleder <peter.muehleder@uni-leipzig.de>
2019-10-15 16:28:18 +0200

    added role and publisher filter for network generation

--- README.md
+++ README.md
@@ -22,9 +22,25 @@ Commands:
 $ python lemongrab.py <COMMAND>
 ```
 
-| command | description |
-| -- | -- |
-| company-dataset | Builds base company dataset with game/release/production role information |
-| wikidata-mapping | Fetches current wikidata mapping (Q-Number <-> Mobygames slug) and country information |
-| company-network | Builds a network of companies based on their common work on games |
-| browser | Opens the lemongrab browser frontend for data exploration |
+# company-dataset
+
+Builds base company dataset with game/release/production role information
+
+# wikidata-mapping 
+
+Fetches current wikidata mapping (Q-Number <-> Mobygames slug) and country information
+
+# company-network 
+
+Builds a network of companies based on their common work on games
+
+| Option | Description | Multiple | Default value | 
+| -- | -- | -- |
+| --country/-c | Release country | X | |
+| --platform/-p | Release platform | X | |
+| --roles/--no-roles | Differentiate companies by their production roles | | --no-roles |
+| --publisher/--no-publisher | Include/exclude publisher roles; only needed when --roles option is set | | --no-publisher |
+
+# browser 
+
+Opens the lemongrab browser frontend for data exploration
--- lemongrab/company_network.py
+++ lemongrab/company_network.py
@@ -8,8 +8,6 @@ WIKIDATA_MAPPING = "datasets/wikidata_mapping.json"
 ID_2_SLUG = "datasets/mobygames_companies_id_to_slug.json"
 
 
-
-
 def load_company_dataset():
     with open(COMPANY_DATASET) as f:
         ds = json.load(f)
@@ -30,6 +28,31 @@ def load_company_dataset():
 
 class CompanyNetworkBuilder():
     
+    """
+    Returns a list of unique ids for a game company when roles should be 
+    considered:
+    ["<COMPANY_ID>__<ROLE1>", "<COMPANY_ID>__<ROLE2>", ... ]
+    otherwhise just return company id:
+    ["COMPANY_ID"]
+    """
+    def company_ids(self, company_id, games):
+        ids = []
+        if self.roles:
+            roles = set([ x["production_role"] for x in games ])
+            for role in roles:
+                if not self.publisher and role == "Published by":
+                    continue
+
+                ids.append("{}__{}".format(company_id, role))
+            return ids
+        else:
+            return [ company_id ]
+
+
+    """
+    Returns a list of company_ids who worked on games released in :countries:
+    and on :platform:
+    """
     def _filter_dataset(self, dataset, countries, platform):
         filtered_ids = []
         for company_id, games in dataset.items():
@@ -43,66 +66,122 @@ class CompanyNetworkBuilder():
             platforms = set()
             for game in games:
                 r_countries.update(game["release_countries"])
-                platforms.update([ game["platform"]])
+                platforms.update([game["platform"]])
         
             c_overlap = len(set(countries).intersection(r_countries))
 
             if platform and not countries:
                 if platform in platforms:
-                    filtered_ids.append(company_id)
+                    filtered_ids += self.company_ids(company_id, games)
+                    #filtered_ids.append(company_id)
+
             elif countries and not platform:
                 if c_overlap > 0:
-                    filtered_ids.append(company_id)
+                    filtered_ids += self.company_ids(company_id, games)
+                    #filtered_ids.append(company_id)
             else:
                 if c_overlap > 0 and platform in platforms:
-                    filtered_ids.append(company_id)
+                    filtered_ids += self.company_ids(company_id, games)
+                    #filtered_ids.append(company_id)
 
         print(len(filtered_ids))
         return filtered_ids
 
 
-    def _filter_games(self, ds, countries, platform):
+    """
+    Returns a list of all games a company worked on based on filter criterias countries, platform and role
+    """
+    def _filter_games(self, ds, countries, platform, role):
         if countries:
             ds = [ x for x in ds if len(set(x["release_countries"]).intersection(set(countries))) > 0 ]
         if platform:
             ds = [ x for x in ds if platform == x["platform"]]
+        if role:
+            ds = [ x for x in ds if x["production_role"] == role]
 
         return set([ x["game_slug"] for x in ds ])
 
-    def __init__(self, countries=None, platform=None):
-        companies = load_company_dataset()
-        slug_map = companies["slug_map"]
+
+    """
+    Returns country information from wikidata for a mobygames compnay id :company_id:
+    Returns "undefined" if no country information is available
+    """
+    def _get_wiki_country(self, company_id):
+        slug = self.companies["slug_map"][company_id]
+        country = ""
+        if slug in self.companies["wiki_map"]:
+            country = self.companies["wiki_map"][slug]["country"]
+        if country:
+            return country
+        else:
+            return "undefined"
+
+
+    def __init__(self, countries=None, platform=None, roles=False, publisher=False):
+
+        self.roles = roles
+        self.publisher = publisher
+
+        self.companies = load_company_dataset()
+        #slug_map = companies["slug_map"]
 
         g = nx.Graph()
         
         all_games = set()
+
         print("generating network graph ...")
-        for c1, c2 in tqdm(combinations(self._filter_dataset(companies["production_details"], countries, platform), 2)):
+        games = {}
+        for c1, c2 in tqdm(combinations(self._filter_dataset(self.companies["production_details"], countries, platform), 2)):
 
-            c1_games = self._filter_games(companies["production_details"][c1], countries, platform)
-            c2_games = self._filter_games(companies["production_details"][c2], countries, platform)
+            if self.roles:
+                c1_id = c1.split("__")[0]
+                c1_role = c1.split("__")[1]
+                c2_id = c2.split("__")[0]
+                c2_role = c2.split("__")[1]
+            else:
+                c1_id = c1
+                c1_role = None
+                c2_id = c2
+                c2_role = None              
 
-            overlap = c1_games.intersection(c2_games)
+            if c1 not in games:
+                games[c1] = self._filter_games(self.companies["production_details"][c1_id], countries, platform, c1_role)
+            else:
+                c1_games = games[c1]
+
+            if c2 not in games:
+                games[c2] = self._filter_games(self.companies["production_details"][c2_id], countries, platform, c2_role)
+            else:
+                c2_games = games[c2]
+
+            overlap = games[c1].intersection(games[c2])
             all_games = all_games.union(overlap)
 
             if len(overlap) > 0:
                 g.add_edge(c1, c2, weight=len(overlap))
         
+        #add noe information
         for node in g.nodes():
-            slug = slug_map[node]
-            country = None
-            if node in companies["wiki_map"]:
-                country = companies["wiki_map"][node]["country"]
-            if not country:
-                country = "undefined"
-            g.nodes[node]["country"] = country
-            g.nodes[node]["label"] = companies["production_details"][node][0]["company_name"]
+            id_ = node.split("__")[0]
+            if self.roles:
+                role = node.split("__")[1]
+
+            g.nodes[node]["country"] = self._get_wiki_country(id_)
+            if self.roles:
+                g.nodes[node]["label"] = self.companies["production_details"][id_][0]["company_name"] + "(" + role + ")"
+            else:
+                g.nodes[node]["label"] = self.companies["production_details"][id_][0]["company_name"]
+            g.nodes[node]["no_of_games"] = len(games[node])
 
         out_file = "company_networks/company_network_"
         if countries:
             out_file += "_".join(countries).replace(" ", "_")
         if platform:
             out_file += "_{}".format(platform.replace(" ","_"))
+        if self.roles:
+            out_file += "_roles"
+        if self.publisher:
+            out_file += "_pub"
         out_file += ".graphml"
 
         print("\nNetwork file saved as: {}\n".format(out_file))
